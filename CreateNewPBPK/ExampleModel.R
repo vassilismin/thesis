553e2143c9919d6dd4dab98756dc6c832b17eff7
Periklis_ts <ptsirostsib@gmail.com>
2019-10-25 18:13:03 +0300

    Update ExampleModel.R

--- CreateNewPBPK/ExampleModel.R
+++ CreateNewPBPK/ExampleModel.R
@@ -1,165 +1,120 @@
 library(rstan)
 library(ggplot2)
 library(parallel)
-library(openxlsx)
 
-setwd("C:/Users/user/Desktop/paper_revision/numerical/multivariate")
+#setwd("C:/Users/user/Desktop/paper_revision/numerical/multivariate")
+#setwd("/home/periklis/Desktop/test")
 
 
-rstan_options(auto_write = TRUE)
-options(mc.cores = parallel::detectCores())
-############################################
-#the order of compartments is as follows:
-#[1]= MU
-#[2]= AD
-#[3]= TE
-#[4]= SK
-#[5]= HT
-#[6]= BR
-#[7]= KI 
-#[8]= RE
-#[9]= ST
-#[10]= SPL 
-#[11]= LU
-#[12]= LI
-#[13]= ART
-#[14]= VEN
+####################################################################
+###################             DATA             ###################
+####################################################################
 
-########################################################################
-predictor<-function(w,gender){
-        k1<-c(9.61e-02,-4.88e-06,3.05e-10,-3.62e-15,1.22e-20,0,0.17)
-        k2<-c(3.95e-02,1.59e-05,-6.99e-10,1.09e-14,-5.26e-20,0,0.05)
-        k3<-c(1.67e-04,6.2e-10,-6.54e-13,2.48e-17,-2.85e-22,1.03e-27,0.001)
-        k4<-c(1.07e-01,-3.26e-06,6.11e-11,-5.43e-16,1.83e-21,0,0.05)
-        k5<-c(8.53e-03,-4.07e-07,1.4e-11,-1.9e-16,1.05e-21,-1.94e-27,0.04)
-        k6<-c(1.19e-01,-3.51e-06,4.28e-11,-1.82e-16,0,0,0.12)
-        k7<-c(7.31e-03,-8.29e-08,5.71e-13,0,0,0,0.19)
-        k8<-rep(0,6)
-        k9<-c(1.88e-03,8.76e-08,-2.52e-12,1.86e-17,0,0,0.01)
-        k10<-c(1.74e-02,-5.3e-07,1.18e-11,-6.74e-17,0,0,0.14)
-        k11<-c(1.67e-02,-9.96e-08,-1.09e-13,1.13e-17,0,0,1)
-        k12<-c(3.49e-02,-3.23e-07,2.13e-12,0,0,0,0.065)
-        k13<-c(3.66e-02,-3.44e-07,5.00e-12, -2.59e-17,0.0,0.0)
-        k14<-c(5.49e-02,-5.15e-07, 7.50e-12,-3.87e-17,0.0,0.0)
-        
-        k1_W<-c(1.17e-01,-3.59e-06,3.19e-10,-3.55e-15,-7.58e-22,0.0)
-        k2_W<-c(5.91e-02,1.20e-05,-5.80e-10,1.12e-14,-6.36e-20,0.0)
-        k3_W<-c(1.94e-04,-8.32e-09,3.15e-13,0.0,0.0,0.0)
-        k4_W<-c(9.54e-02,-1.7e-06,-1.64e-13,2.64e-16,-1.49e-21,0.0)
-        k5_W<-c(5.72e-03,-1.02e-07,2.53e-12,-2.71e-17,9.29e-23,0.0)
-        k6_W<-c(1.12e-01,-3.33e-06,4.04e-11,-1.70e-16,0.0,0.0)
-        k7_W<-c(8.04e-03,-1.38e-07,2.19e-12,-1.34e-17,0.0,0.0)
-        k8_W<-rep(0,6)
-        k9_W<-c(1.88e-03,8.76e-08,-2.52e-12,1.86e-17,0.0,0.0)
-        k10_W<-c(1.89e-02,-6.62e-07,1.56e-11,-9.87e-17,0.0,0.0)
-        k11_W<-c(1.74e-02,-7.14e-08,-6.78e-14,0.0,0.0,0.0)
-        k12_W<-c(3.59e-02,-4.76e-07,8.50e-12,-5.45e-17,0.0,0.0)
-        k13_W<-c(3.66e-02,-3.44e-07,5.00e-12, -2.59e-17,0.0,0.0)
-        k14_W<-c(5.49e-02,-5.15e-07, 7.50e-12,-3.87e-17,0.0,0.0)
-        
-        density <- rep(1,14)
-        flow_frac<-c(0.17,0.05,0.005,0.05,0.04,0.12,0.19,0,0.01,0.14,1,0.065)
-        flow_frac_w<-c(0.17,0.05,0.002,0.05,0.04,0.12,0.19,0,0.01,0.14,1,0.065)
-        
-        
-        const<-list(k1,k2,k3,k4,k5,k6,k7,k8,k9,k10,k11,k12,k13,k14)
-        const_W<-list(k1_W,k2_W,k3_W,k4_W,k5_W,k6_W,k7_W,k8_W,k9_W,k10_W,k11_W,k12_W,k13_W,k14_W)
-        vol<-rep(0,14)
-        flow<-rep(0,12)
-        TF<-(187*(w)^0.81)*60/1000 #Total flow
-        weight <- w
-        
-        if (gender==0){
-                if (w>75){
-                        w<-75*1000
-                }
-                else{
-                        w<-w*1000
-                }
-                co<-const
-                for (i in 1:14){
-                        
-                        vol[i]<-(co[[i]][1]+co[[i]][2]*w+co[[i]][3]*w^2+co[[i]][4]*w^3+co[[i]][5]*w^4+co[[i]][6]*w^5)
-                        vol[i]<-vol[i]*weight/density[i]
-                }
-        }
-        
-        else if (gender==1){
-                if (w>65){
-                        w<-65*1000
-                }
-                else{
-                        w<-w*1000
-                }
-                co<-const_W
-                for (i in 1:14){
-                        
-                        vol[i]<-(co[[i]][1]+co[[i]][2]*w+co[[i]][3]*w^2+co[[i]][4]*w^3+co[[i]][5]*w^4+co[[i]][6]*w^5)
-                        vol[i]<-vol[i]*weight/density[i]
-                }
-        }
-        
-        
-        
-        for (i in 1:12){
-          if (gender==0){
-            flow[i]<-TF*flow_frac[i]
-          }
-          else{
-            flow[i]<-TF*flow_frac_w[i]
-          }
-        }
-        
-        flow[8]<-TF-sum(flow)+TF
-        vol[8]<-weight-sum(vol)
-        combine<-c(flow,vol)
-        
-        return(combine)
-        
-}
-###############################################################################################
-Kp <- c(0.56, 3.34, 0.76, 0.46, 0.86, 0.32, 0.73, 2.2, 0.75, 0.53, 0.71, 1.35) 
-T_inf <- (1/60)/2
-FUB<-0.015/0.65
+dose <- 1.7e03 #micro grams
 
-#load data
-diaz_data <- read.xlsx("diaz_data_missing.xlsx", colNames = TRUE,rowNames = TRUE)
-n_physio <- 3 #physiological characteristics: age, gender, weight
-concen_missing <- as.matrix(diaz_data[2:(dim(diaz_data)[1]-n_physio),])#matrix of concentration-time profiles with missing values
-time <-  c(5/60, 0.25, 0.5, 0.75, 1.0, 1.5, 2.0, 3.0, 4.0, 6.0, 8.0, 10.0, 12.0, 24.0, 30.0, 36.0, 48.0, 72.0)
+# tissue weights (in g)
+W_tot <-253.4 # ;body weight, experimental data - g
+W_lu <-1.2 # weight of lungs, experimental data - g
+W_bm <- W_tot*0.03  # weight of bone marrow, literature (Travlos 2006) - g
+W_bm_exp <- 0.02 # weight of extracted bone marrow 
+W_br <- 1.35 # weight of brain, experimental data - g
+W_ht <- 0.87 # weight of heart, experimental data - g
+W_ki <- 2.37 # weight of kidneys, experimental data - g
+W_li <- 10.03 # weight of liver, experimental data - g 
+W_spl <- 0.78 # weight of spleen, experimental data - g
+W_re <- 212.8 # weight of rest of the body, experimental data - g
 
+# Weight of capillary blood assuming density = 1
+# (capillary blood is given as a percentage of tissue volume)
+Wb_lu <- 0.36 * W_lu # weight of blood in lungs, literature (Brown et al., 1997) - g
+Wb_bm <- 0.1* W_bm # weight of blood in bone marrow, estimated - g
+Wb_br <-  0.07 * W_br  # weight of blood in brain, literature (Brown et al., 1997) - g
+Wb_ht <- 0.26 * W_ht # weight of blood in heart, literature (Brown et al., 1997) - g   
+Wb_ki <- 0.16 * W_ki # weight of blood in kidneys, literature (Brown et al., 1997) - g 
+Wb_li <-  0.21 * W_li # weight of blood in liver, literature (Brown et al., 1997) - g
+Wb_spl <- 0.22 * W_spl # weight of blood in spleen, literature (Brown et al., 1997) - g
+Wb_re <- 0.04 * W_re # weight of blood in rest of the body, literature (Brown et al., 1997) - g 
+# weight of arterial and venous blood, experimental data and literature - g
+W_blood <- 0.065 * W_tot - (Wb_spl + Wb_li + Wb_lu + Wb_br + Wb_ht + Wb_ki + Wb_bm + Wb_re)
+
+#Regional blood flows (in mL per hour)
+fQs = 0.0146 # fraction of cardiac output to spleen, literature (Bernareggi and Rowland, 1991) - unitless
+fQl = 0.183 # fraction of cardiac output to liver, literature (chapter 5) - unitless
+fQbr = 0.02	# fraction of cardiac output to brain, literature (chapter 5) - unitless
+fQh = 0.051 # fraction of cardiac output to heart, literature (chapter 5) - unitless
+fQk = 0.141 # fraction of cardiac output to kidneys, literature (chapter 5) - unitless
+fQbm = 0.0267 # fraction of cardiac output to bone marrow, literature (Brookes, 1967) - unitless
+fQrest = 1-fQs-fQl-fQbr-fQh-fQk-fQbm # fraction of cardiac output to rest of the body, fitted value - unitless
+Q_tot <- 4980 # cardiac output, literature (chapter 5) - mL/h
+Q_bm <- fQbm*Q_tot # blood flow to bone marrow - mL/h
+Q_br <- fQbr*Q_tot # blood flow to brain - mL/h		
+Q_ht <- fQh*Q_tot	# blood flow to heart - mL/h
+Q_ki <- fQk*Q_tot # blood flow to kidneys - mL/h
+Q_spl <- fQs*Q_tot # blood flow to spleen - mL/h		
+Q_li <- fQl*Q_tot	# blood flow to liver - mL/h
+Q_re <- fQrest*Q_tot # blood flow to rest of the body - mL/h
+
+params <- c(W_tot,W_lu,  W_bm, W_br, W_ht, W_ki, W_li, W_spl, W_re, Wb_lu, Wb_bm, Wb_br,Wb_ht,
+            Wb_ki, Wb_li, Wb_spl, Wb_re, W_blood, Q_tot, Q_bm, Q_br, Q_ht, Q_ki, Q_spl, Q_li, Q_re,
+            dose)
+
+#####################
+# Priors #
+#####################
+#PCs uptake capacity per organ weight (in micrograms per g of tissue)
+M_lu_cap <- 25.5 # maximum phagocytizing cells uptake per lung weight, fitted value - ug/g
+M_bm_cap <- 41.2 # maximum phagocytizing cells uptake per bone marrow weight, fitted value - ug/g
+M_br_cap <- 0.0827 # maximum phagocytizing cells uptake per brain weight, fitted value - ug/g
+M_ht_cap <- 5.03 # maximum phagocytizing cells uptake per heart weight, fitted value - ug/g
+M_ki_cap <- 1.08 # maximum phagocytizing cells uptake per kidney weight, fitted value - ug/g
+M_li_cap <- 74.8 # maximum phagocytizing cells uptake per liver weight, fitted value - ug/g  
+M_spl_cap <- 631 # maximum phagocytizing cells uptake per spleen weight, fitted value - ug/g
+M_blood_cap <- 0.0396 # maximum phagocytizing cells uptake per blood weight, fitted value - ug/g
+M_re_cap <- 17.6 # maximum phagocytizing cells uptake per slowly perfused tissue weight, fitted value - ug/g
+
+x_fast <-1.06e-03 # permeability coefficient from blood to fast perfused tissue, fitted value - unitless
+x_br <-0 # permeability coefficient from blood to brain tissue, fitted value - unitless
+x_re <-8.25e-05 # permeability coefficient from blood to rest of the body, fitted value - unitless
+P <-0.147 # partition coefficient tissue:blood, fitted value - unitless
+k_ab0 <- 16.1 # maximum uptake rate by phagocytizing cells, fitted value - 1/h
+k_ab0_spl  <- 0.112 # maximum uptake rate by phagocytizing cells in spleen, fitted value - 1/h
+k_de <- 4.9e-19 # desorption rate by phagocytizing cells, fitted value - 1/h   
+CLE_f <- 1.18e-02 # clearance rate to feces from liver, fitted value - mL/h
+CLE_u <- 6.56e-03 # clearance rate to urine from blood in kidneys, fitted value - 1/h
+frbr <- 0.346 # fraction of capillary blood remained in brain when measured - unitless
+fro <- 0.177 # fraction of capillary blood remained in other organs when measured - unitless
+
+# Time of sampling
+time <-  c(10/60, 1, 24, 24*7, 24*28, 24*56)
+#read data
+data <- openxlsx::read.xlsx("elgrabli_data.xlsx", sheet = 1, colNames = T, rowNames = T)
+# Discard control, urine (for the time) and the first row which is the compartment names 
+# and also transpose the data so that each column represents a compartment
+TransformedData <- t(as.matrix(sapply(data, as.numeric))[1:8,2:7])
+# Retrieve the compartment names
+colnames(TransformedData) <- rownames(data[1:8,])
 #create an array to store the number of samples of each patient
-samp <- apply(concen_missing, 2, function(x) length(na.omit(x)))
-#collapse concentration matrix to a vector and remove missing values
-concen <- as.vector((concen_missing))
+Nsamp <- apply(TransformedData, 2, function(x) length(na.omit(x)))
 
-# Create the corresponding time vector
+# Create a vectorized form of the data and a corresponding time vector
 time_vec <- list()
-concen <- list()
-for (i in 1:dim(diaz_data)[2]){
-  time_vec[[i]] <- time[-which(is.na(concen_missing[,i]))]
-  concen[[i]] <- concen_missing[-which(is.na(concen_missing[,i])),i]
+MassData <- list()
+for (i in 1:dim(TransformedData)[2]){
+  if (Nsamp[i] != length(time)){
+  time_vec[[i]] <- time[-which(is.na(TransformedData[,i]))]
+  MassData[[i]] <- TransformedData[-which(is.na(TransformedData[,i])),i]
+  } else {
+    time_vec[[i]] <- time
+    MassData[[i]] <- TransformedData[,i]
+  }
 }
+# Unlist the lists and convert them to vectors
 time_vec <- as.vector(unlist(time_vec))
-concen <- as.vector(unlist(concen))
-
-
-#create a variable to store for each patient the number of samples
-gender <- array(unlist(diaz_data[22,]))
-weight <- array(unlist(diaz_data[21,]))
-
-# weight adjusted dose with min dose 5 mg and max 10 mg (produced values in ng)
-dose <- 5e+06 + (10-5)*(weight-min(weight)) /(max(weight)-min(weight))*1e06
-
-#create a matrix containing the parameters of all patients
-params <- matrix(rep(0, 41*dim(diaz_data)[2]), ncol=23)
-for (i in 1:dim(diaz_data)[2]){
-  params[,i] <- c(predictor(weight[i],gender[i]), Kp, FUB, T_inf, dose[i])
-}
+MassData <- as.vector(unlist(MassData))
 
 #############################################################################################
 
-Diazepam_dat<-list(
+DataList <- list(
                    eta_tr = c(70.8,1,3.34), #cl, a (kp coeff), kp_fat,kp_rest
                    eta_tr_std = c(62.5,1,0.44), 
                    R = 0.65,
@@ -182,7 +137,7 @@ Diazepam_dat<-list(
 
 tic = proc.time()
 
-fit <- stan(file = 'multivar2.stan', data = Diazepam_dat, 
+fit <- stan(file = 'test1.stan', data = DataList, 
             iter = 5400, warmup=400, chains=4)
 options(max.print=5.5E5) 
 
